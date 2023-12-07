b9c93d0a7d9a14bb4a1a97cdb9b35766dcce67e7
Alise Cervantes <alise09@icloud.com>
2023-12-07 07:04:11 -0800

    Express Session Part 2

--- app.js
+++ app.js
@@ -6,13 +6,6 @@ var logger = require("morgan");
 var session = require("express-session");
 var FileStore = require("session-file-store")(session);
 
-const campsiteRouter = require("./routes/campsiteRouter");
-const promotionRouter = require("./routes/promotionRouter");
-const partnerRouter = require("./routes/partnerRouter");
-
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-
 var app = express();
 
 // view engine setup
@@ -39,35 +32,16 @@ app.use(
   })
 );
 
-// Updated Authentication Middleware to use sessions
+// Authentication Middleware - Updated
 function auth(req, res, next) {
   console.log(req.session);
 
   if (!req.session.user) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-      const err = new Error("You are not authenticated!");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      return next(err);
-    }
-
-    const auth = Buffer.from(authHeader.split(" ")[1], "base64")
-      .toString()
-      .split(":");
-    const user = auth[0];
-    const pass = auth[1];
-    if (user === "admin" && pass === "password") {
-      req.session.user = "admin";
-      return next(); // authorized
-    } else {
-      const err = new Error("You are not authenticated!");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      return next(err);
-    }
+    const err = new Error("You are not authenticated!");
+    err.status = 401;
+    return next(err);
   } else {
-    if (req.session.user === "admin") {
+    if (req.session.user === "authenticated") {
       return next();
     } else {
       const err = new Error("You are not authenticated!");
@@ -77,11 +51,16 @@ function auth(req, res, next) {
   }
 }
 
-app.use(auth);
-
 // Routers
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const campsiteRouter = require("./routes/campsiteRouter");
+const promotionRouter = require("./routes/promotionRouter");
+const partnerRouter = require("./routes/partnerRouter");
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use(auth); // Ensure auth middleware is used after the usersRouter
 app.use("/campsites", campsiteRouter);
 app.use("/promotions", promotionRouter);
 app.use("/partners", partnerRouter);
