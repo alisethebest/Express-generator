46d9c9a7e7c4203918b6449426d37ab0f429c7fd
Alise Cervantes <alise09@icloud.com>
2023-12-18 18:40:02 -0800

    HTTPS amd Secure Communication

--- app.js
+++ app.js
@@ -30,6 +30,16 @@ app.use(
   })
 );
 
+// Secure traffic only
+app.all('*', (req, res, next) => {
+    if (req.secure) {
+      return next();
+    } else {
+        console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
+        res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
+    }
+});
+
 // Passport initialization
 app.use(passport.initialize());
 app.use(passport.session());
